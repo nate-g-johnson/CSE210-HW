546dd7df4766561f498f3a087dc7a19ec5ea6e1d
nate-g-johnson <n_george_j@outlook.com>
2025-05-22 08:43:15 -0400

    Fractions program

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,54 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    // Getters and Setters
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public void SetNumerator(int value)
+    {
+        _numerator = value;
+    }
+
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public void SetDenominator(int value)
+    {
+        _denominator = value;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+}
