5b43896b9ebc1cbd2f822354e0f38d62b278ed32
nate-g-johnson <n_george_j@outlook.com>
2025-06-16 13:20:47 -0400

    date

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -8,8 +8,8 @@ class Program
         List<Activity> activities = new List<Activity>
         {
             new Running("03 Nov 2022", 30, 3.0),
-            new Cycling("04 Nov 2022", 45, 15.0),
-            new Swimming("05 Nov 2022", 40, 30)
+            new Cycling("03 Nov 2022", 45, 15.0),
+            new Swimming("03 Nov 2022", 40, 30)
         };
 
         foreach (Activity activity in activities)
